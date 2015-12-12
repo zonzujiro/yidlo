062c34c83b77453a6bd09cc2a70084318dca087d
zonzujiro <zonzujiro@gmail.com>
2015-12-12 10:58:11 +0200

    testing geo

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 window.onload = function () {
     var elem = document.getElementById("result");
-    var yKey = "007d1580-2af8-4055-ac77-d4e07172b230";
+    elem.innerHTML = "<h1>" + ["Salateira", "McDonalds", "Wokkery", "Пелотка", "Пузата Хата"][Math.floor(Math.random() * 5)] + "</h1>";
 
-    navigator.geolocation.getCurrentPosition(function(data) {
-        var lat = data.coords.latitude;
-        var lng = data.coords.longitude;
-
-        $.getJSON("https://search-maps.yandex.ru/v1/?text=где%20поесть&type=biz&lang=uk_UA&ll=" + lat + "%2C" + lng + "&sspn=0.06791&apikey=" + yKey, {}, function (data) {
-            console.log(data);
-        });
-    });
+    ymaps.ready(init);
 
     function init(){
         ymaps.geolocation.get().then(function (res) {
@@ -35,8 +28,4 @@ window.onload = function () {
             console.log(e);
         });
     }
-
-    Math.seedrandom(Math.floor(new Date().getTime() / 86400000));
-    elem.innerHTML = "<h1>" + ["Salateira", "McDonalds", "Wokkery", "Пелотка", "Пузата Хата"][Math.floor(Math.random() * 5)] + "</h1>";
-    ymaps.ready(init);
 };
