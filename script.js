b9d7658fa28fa68e96f8d26b0214f48698372b61
zonzujiro <zonzujiro@gmail.com>
2015-12-12 10:56:44 +0200

    testing geo

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.onload = function () {
                 ),
                 map = new ymaps.Map('map', {
                     center: mapState.center,
-                    zoom: 17
+                    zoom: 16
                 }),
                 myGeoObject = new ymaps.GeoObject({
                     geometry: {
@@ -29,41 +29,12 @@ window.onload = function () {
                         coordinates: mapState.center
                    }
                 });
-                // lat = bounds[0][0],
-                // lng = bounds[1][0];
-
-                // $.getJSON(config.apiUrl + 'v2/venues/explore?ll=' + lat + ',' + lng + '&oauth_token=' + window.token + '&v=20140601', {}, function(data) {
-                //     venues = data['response']['groups'][0]['items'];
-                //     /* Place marker for each venue. */
-                //     for (var i = 0; i < venues.length; i++) {
-                //     /* Get marker's location */
-                //         var latLng = new L.LatLng(
-                //             venues[i]['venue']['location']['lat'],
-                //             venues[i]['venue']['location']['lng']
-                //         );
-                //         /* Build icon for each icon */
-                //         var fsqIcon = venues[i]['venue']['categories'][0]['icon'];
-                //         var leafletIcon = L.Icon.extend({
-                //             iconUrl: fsqIcon['prefix'] + '32' + fsqIcon['suffix'],
-                //             shadowUrl: null,
-                //             iconSize: new L.Point(32,32),
-                //             iconAnchor: new L.Point(16, 41),
-                //             popupAnchor: new L.Point(0, -51)
-                //         });
-                //         var icon = new leafletIcon();
-                //         var marker = new L.Marker(latLng, {icon: icon})
-                //             .bindPopup(venues[i]['venue']['name'], { closeButton: false })
-                //             .on('mouseover', function(e) { this.openPopup(); })
-                //             .on('mouseout', function(e) { this.closePopup(); });
-                //         map.addLayer(marker);
-                //     }
-                // })
-
+                
                 map.geoObjects.add(myGeoObject);
-            }, function (e) {
-                console.log(e);
-            });
-        }
+        }, function (e) {
+            console.log(e);
+        });
+    }
 
     Math.seedrandom(Math.floor(new Date().getTime() / 86400000));
     elem.innerHTML = "<h1>" + ["Salateira", "McDonalds", "Wokkery", "Пелотка", "Пузата Хата"][Math.floor(Math.random() * 5)] + "</h1>";
