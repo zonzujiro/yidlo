7744e4ecbe2e8c4d76606926fd7ee2ea8a6a4d43
zonzujiro <zonzujiro@gmail.com>
2015-12-13 11:26:16 +0200

    Added geo searching

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
-window.onload = function () {
+window.onload = function() {
     var elem = document.getElementById("result");
     var apiKey = "007d1580-2af8-4055-ac77-d4e07172b230";
 
-    navigator.geolocation.getCurrentPosition(function(position) {
-        $.getJSON("https://search-maps.yandex.ru/v1/?text=где%20поесть&type=biz&lang=uk_UA&ll=" + position.coords.latitude + "%2C" + position.coords.longitude + "&sspn=0.013583%2C0.005685&apikey=" + apiKey, {}, function (data) {
-            console.log(data);
-        });
-    });
-
-    function init(){
+    function init() {
         ymaps.geolocation.get().then(function (res) {
             var $container = $('map'),
                 bounds = res.geoObjects.get(0).properties.get('boundedBy'),
                 mapState = ymaps.util.bounds.getCenterAndZoom(
-                    bounds,
-                    [$container.width(), $container.height()]
+                    bounds, [$container.width(), $container.height()]
                 ),
                 map = new ymaps.Map('map', {
                     center: mapState.center,
-                    zoom: 16
+                    zoom: 16,
+                    controls: []
                 }),
-                myGeoObject = new ymaps.GeoObject({
+                user = new ymaps.GeoObject({
                     geometry: {
                         type: "Point",
                         coordinates: mapState.center
-                   }
+                    }
                 });
 
-                console.log("ll=" + mapState.center[1].toFixed(6) + "%2C" + mapState.center[0].toFixed(6));
-
-                console.log($.getJSON("https://search-maps.yandex.ru/v1/?text=еда&type=biz&lang=uk_UA&ll=" + mapState.center[0].toFixed(6) + "%2C" + mapState.center[1].toFixed(6) + "&spn=0.013583%2C0.005685&apikey=" + apiKey, {}, function (data) {
-                    console.log(data);
-                }));
-
-                map.geoObjects.add(myGeoObject);
-        }, function (e) {
+            $.getJSON("https://search-maps.yandex.ru/v1/?text=%D0%93%D0%B4%D0%B5%20%D0%BF%D0%BE%D0%B5%D1%81%D1%82%D1%8C&type=biz&lang=uk_UA&ll=" + mapState.center[1] + "," + mapState.center[0] + "&spn=0.013583%2C0.005685&apikey=" + apiKey, {}, function (data) {
+                var lunch = data.features[Math.floor(Math.random() * data.features.length)];
+                console.log(lunch);
+                elem.innerHTML = "<h1>" + lunch.properties.name + "</h1>";
+                map.geoObjects
+                        .add(user)
+                        .add(new ymaps.Placemark([lunch.geometry.coordinates[1], lunch.geometry.coordinates[0]], {
+                            balloonContent: lunch.properties.name
+                        }, {
+                            preset: 'islands#circleIcon',
+                            iconColor: '#3caa3c'
+                        }));
+            });
+        }, function(e) {
             console.log(e);
         });
     }
 
     Math.seedrandom(Math.floor(new Date().getTime() / 86400000));
-    elem.innerHTML = "<h1>" + ["Salateira", "McDonalds", "Wokkery", "Пелотка", "Пузата Хата"][Math.floor(Math.random() * 5)] + "</h1>";
     ymaps.ready(init);
 };
