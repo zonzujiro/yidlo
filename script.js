e0d414e9d2db7ea5c9f97f68fdb165cf98d2f9a1
zonzujiro <zonzujiro@gmail.com>
2015-12-13 15:22:57 +0200

    Added message when there is no results found

--- script.js
+++ script.js
@@ -62,7 +62,10 @@ window.onload = function() {
                 });
 
             $.getJSON("https://search-maps.yandex.ru/v1/?text=%D0%93%D0%B4%D0%B5%20%D0%BF%D0%BE%D0%B5%D1%81%D1%82%D1%8C&type=biz&rspn=1&lang=uk_UA&ll=" + mapState.center[1] + "," + mapState.center[0] + "&spn=0.013583%2C0.005685&apikey=" + apiKey, {}, function (data) {
-                if ()
+                if (data.features.length == 0) {
+                    elem.innerHTML = "<h1>Вибачь, проте поруч не має навіть вшивого генделя. Йти нікуди.</h1>";
+                    return;
+                }
 
                 var lunch = data.features[Math.floor(Math.random() * data.features.length)];
                 
@@ -75,6 +78,7 @@ window.onload = function() {
                             preset: 'islands#circleIcon',
                             iconColor: '#3caa3c'
                         }));
+
             });
         }, function(e) {
             console.log(e);
